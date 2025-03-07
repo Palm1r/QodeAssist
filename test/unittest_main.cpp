9a5047618dad3166a3eaaaba84cefc1d0c3906ad
Povilas Kanapickas <povilas@radix.lt>
2025-03-07 02:58:09 +0200

    chore: Silence Qt warnings during tests (#110)

--- test/unittest_main.cpp
+++ test/unittest_main.cpp
@@ -19,9 +19,19 @@
 
 #include <gtest/gtest.h>
 #include <QGuiApplication>
+#include <QtGlobal>
+
+void silenceQtWarningNoise(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    if (msg.startsWith("SOFT ASSERT") || msg.startsWith("Accessing MimeDatabase")) {
+        return;
+    }
+    qt_message_output(type, context, msg);
+}
 
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(silenceQtWarningNoise);
     QGuiApplication application(argc, argv);
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
